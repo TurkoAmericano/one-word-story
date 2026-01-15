0c0075c6a1642d6c46d239513e5fb2f533716d06
TurkoAmericano <cuerhart@yahoo.com>
2026-01-15 18:13:36 +0300

    Force rebuild

--- deploy/rebuild.sh
+++ deploy/rebuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && docker-compose -f docker-compose.prod.yml build && docker-compose -f docker-compose.prod.yml up -d
+git pull && docker-compose -f docker-compose.prod.yml build --no-cache && docker-compose -f docker-compose.prod.yml up -d
